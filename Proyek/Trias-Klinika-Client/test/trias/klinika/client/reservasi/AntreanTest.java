88019c8140bad1adeddd78c332dc21c5b364c79f
istihamin fitriyah <iminimanamin@gmail.com>
2015-05-04 19:35:51 -0700

    Progres 4.1
    Do : menambahkan JUnit
    To do : menambah antrean
    Hambatan : masih merah JUnitnya

---
+++ Proyek/Trias-Klinika-Client/test/trias/klinika/client/reservasi/AntreanTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trias.klinika.client.reservasi;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class AntreanTest {
+    
+    public AntreanTest() {
+    }
+
+    @org.junit.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of awal method, of class Antrean.
+     */
+    @org.junit.Test
+    public void testAwal() {
+        System.out.println("awal");
+        Antrean instance = null;
+        instance.awal();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
